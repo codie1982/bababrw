6f9d524271ea1d4d9b021d18a1c7a95ab50504b1
Engin EROL <engin_erol@hotmail.com>
2023-11-03 15:42:23 +0300

    Bağımlılıklar eklende ve gradle dosyaları düzenlendi

---
+++ app/src/main/java/com/grnt/bababrowser001/BrowserApplication.kt
@@ -0,0 +1,6 @@
+package com.grnt.bababrowser001
+
+import android.app.Application
+
+class BrowserApplication : Application() {
+}
---
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -0,0 +1,4 @@
+
+object AndroidComponents {
+    const val VERSION = "121.0.20231101160213"
+}
